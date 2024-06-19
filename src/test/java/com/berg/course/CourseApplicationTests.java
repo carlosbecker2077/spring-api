47f948186f7b067f92e91156367ddb3312e87a65
Carlos Becker <carlos.lemos@dataside.com.br>
2024-06-19 18:35:09 -0300

    first commit

---
+++ src/test/java/com/berg/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berg.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
