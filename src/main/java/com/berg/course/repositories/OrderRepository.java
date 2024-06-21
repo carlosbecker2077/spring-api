2b5f50d37eb3af9f4f30490306d1ce3baf45ad63
Carlos Becker <carlos.lemos@dataside.com.br>
2024-06-21 08:32:17 -0300

    order respository and controller + quality changes

--- src/main/java/com/berg/course/repositories/OrderRepository.java
+++ src/main/java/com/berg/course/repositories/OrderRepository.java
@@ -1,8 +1,9 @@
 package com.berg.course.repositories;
 
+import com.berg.course.entities.Order;
 import com.berg.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//public interface OrderRepository extends JpaRepository<Order, Long> {
-//
-//}
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
