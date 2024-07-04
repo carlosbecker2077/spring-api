8bfd6ff1f34e4047fc533cd81c5eab17c1fbaab4
Carlos Becker <carlos.lemos@dataside.com.br>
2024-07-04 15:15:04 -0300

    update exception handling

--- src/main/java/com/berg/course/services/UserService.java
+++ src/main/java/com/berg/course/services/UserService.java
@@ -2,8 +2,12 @@ package com.berg.course.services;
 
 import com.berg.course.entities.User;
 import com.berg.course.repositories.UserRepository;
+import com.berg.course.services.exceptions.DatabaseException;
 import com.berg.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,13 +33,25 @@ public class UserService {
     }
 
     public void deleteUser(Long id) {
-        this.userRepository.deleteById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User updateUser(Long id, User user) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User user) {
